3e8b01d32363c8137d6d09baf6567d3891199d9a
Sachinkisna <sachinsoftware0001@gmail.com>
2024-12-04 17:22:50 +0530

    initial commit2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("While Loop");
-
+        int i=0;
+        while(i<=5){
+            System.out.println("hare krishna");
+            i++;
+        }
     }
 }
